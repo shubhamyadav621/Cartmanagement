5b6d789d77074e1b266361ebb06113f2aae1ff3e
shubham_ncu <shubham22csu444@ncuindia.edu>
2025-11-08 11:55:02 +0530

    commit for all

--- productservice/target/classes/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE product (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description VARCHAR(500),
-    price DOUBLE NOT NULL,
-    quantity INT NOT NULL
-);
